0a98dc149b90132482819b65c089ece7cb0361f0
Fermin Ramos <ramosfer@unm.edu>
2024-10-29 14:53:57 -0600

    Added a global file to keep track of global vars

---
+++ NBA Database/Database Updater/Global.py
@@ -0,0 +1,6 @@
+# This file contains all global vars used for multiple files.
+#  Should make it easier to change settings since everything will
+#  reference this file.
+
+# Path to our local database within our repository
+BASE_PATH = "/Users/speak_easy/Python UNM/NBA-Parlay-Bets/NBA Database"
